3aa26474d8966e3dcd085be03fc8bfeeb579bb45
Peter Čagala <petercagala@gmail.com>
2021-01-20 05:06:25 +0100

    23.1 Mapped Types

--- Section-2-TypeScript/src/MappedTypes/index.ts
+++ Section-2-TypeScript/src/MappedTypes/index.ts
@@ -15,9 +15,24 @@ type MyMappedType2<T> = {
     [P in keyof T]: T[P];
 }
 
-type MyNewType2 = MyMappedType2<{a: boolean, b: string}>;
+interface IMappedType2 {
+    myProperty1: boolean;
+    myProperty2: string;
+    // readonly myProperty1?: boolean | null;
+    // readonly myProperty2?: string | null;
+}
+
+// alebo
+// type MyNewType2 = MyMappedType2<{a: boolean; b: string}>;
+type MyNewType2 = MyMappedType2<IMappedType2>;
 
 const myObject2: MyNewType2 = {
-    a: true,
-    b: "Katka",
+    myProperty1: true,
+    myProperty2: "Katka",
+};
+
+// alebo uplne piramociaro
+const myObject3: MyMappedType2<IMappedType2> = {
+    myProperty1: true,
+    myProperty2: "Katka",
 };
