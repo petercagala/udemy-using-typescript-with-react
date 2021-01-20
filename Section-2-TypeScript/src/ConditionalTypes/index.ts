6f9313de198c11fcc2e56548cb1c07fa3f395258
Peter Čagala <petercagala@gmail.com>
2021-01-20 06:37:19 +0100

    24.1 Conditional Types

---
+++ Section-2-TypeScript/src/ConditionalTypes/index.ts
@@ -0,0 +1,34 @@
+// type SomeType = "string";
+type SomeType = number;
+type ConditionalType = SomeType extends string ? string : null;
+
+function someFunction<T>(param: T): (someArg: T extends boolean ? 'TypeA' : 'TypeB') => void {
+    const someOtherFunction = (someArg: T extends boolean ? 'TypeA' : 'TypeB'): void => {
+        const a: 'TypeA' | 'TypeB' = someArg;
+    };
+    return someOtherFunction;
+}
+
+someFunction(true)("TypeA");
+someFunction(3)("TypeB");
+
+// **********************************************************
+// never is type, which never happen
+type StringOrNot<T> = T extends string ? string : never;
+
+// it is string | number
+type AUnion = string | number | never;
+
+
+// type Exclude<T, U> = T extends U ? never : T;
+/**
+ * each type of T compare with whole type 'a' | 'b'
+ * 'a' extends 'a' | 'b' ? never : 'a' => never .........a mozem priradit rodicovi a takze true
+ * 'b' extends 'a' | 'b' ? never : 'b' => never
+ * 'c' extends 'a' | 'b' ? never : 'c' => c
+ * 'd' extends 'a' | 'b' ? never : 'd' => d
+ */
+type ResultType = Exclude<'a'| 'b'| 'c' | 'd', 'a' | 'b'>;
+
+type MyType1<T> = T extends number | string ? T : never;
+type MyResults1 = MyType1<number | string | boolean>;
